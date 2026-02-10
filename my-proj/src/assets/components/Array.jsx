e50095a30ed54e4541130b0cec314a479cbb6aec
jerry7262 <rajaabhinay04@gmail.com>
2026-02-10 19:42:13 +0530

    array update

--- my-proj/src/assets/components/Array.jsx
+++ my-proj/src/assets/components/Array.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 
 const Array = ({shoes, rs}) => {
+
+   const [num, setNum] = useState('Hello')
+   const handleUpper = ()=>{
+    setNum(num.toUpperCase())
+   }
+
   return (
+    <>
     <div>
      <p>name of brand:{shoes}</p>
      <p>price of shoes :{rs}</p>
     </div>
+     <div>
+    <p>initial value:{num}</p>
+    <button onClick={handleUpper}>Click</button>
+     </div>
+     </>
   )
 }
 
