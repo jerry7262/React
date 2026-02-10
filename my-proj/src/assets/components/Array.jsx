8eb2a462e1d5f708d824077e93b4483d5041ad68
jerry7262 <rajaabhinay04@gmail.com>
2026-02-10 19:25:26 +0530

    updates

---
+++ my-proj/src/assets/components/Array.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Array = ({shoes, rs}) => {
+  return (
+    <div>
+     <p>name of brand:{shoes}</p>
+     <p>price of shoes :{rs}</p>
+    </div>
+  )
+}
+
+export default Array;
---
+++ my-proj/src/assets/components/Index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Array from './array';
+const Index = () => {
+    const brand = "Jordan";
+    const price = 25000
+  return (
+    <div>
+      <Array shoes={brand}rs={price}/>
+    </div>
+  )
+}
+
+export default Index;
